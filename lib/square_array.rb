04ade39fdd14be65119ded5f22e41bd1402af0e4
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-21 13:33:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   i = 0 
   new_array = []
-  while i < array.length 
-  new_array.push(array[i] ** 2)
-  i += 1
+    while i < array.length 
+    new_array.push(array[i] ** 2)
+    i += 1
+    end
 end
